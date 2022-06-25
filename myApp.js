2f7fb3dccd7d8cdb8c6ae70d8f59c5ad7b5c45f5
Sahil <sohil170246@gmail.com>
2022-06-25 19:56:27 +0530

    mongocoursestrated

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 
 const mongoose = require("mongoose");
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=>console.log('CONNECTED'))
+    .then(()=>{
+        app.listen(5000);
+    }).catch((err)=>console.log(err));
+    
 let Person;
 
 const createAndSavePerson = (done) => {
